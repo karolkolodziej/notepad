0f61ba503cb8a323a329fb81747dd5843fe1eb54
Karol <karolkolodziej01@gmail.com>
2020-02-20 16:14:24 +0100

    Updates

---
+++ precache-manifest.93b54b75d7c0170610feccebdf6e87ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4020ed87325ffd5c1ad3a622117ea02",
+    "url": "/notepad/index.html"
+  },
+  {
+    "revision": "008e61c126d33de4a5ae",
+    "url": "/notepad/static/css/main.f962a385.chunk.css"
+  },
+  {
+    "revision": "e06052c4c7e1c8ad8c9f",
+    "url": "/notepad/static/js/2.98e2246c.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/notepad/static/js/2.98e2246c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008e61c126d33de4a5ae",
+    "url": "/notepad/static/js/main.71d65172.chunk.js"
+  },
+  {
+    "revision": "f9e5273cd11e93edfaf4",
+    "url": "/notepad/static/js/runtime-main.4b496c65.js"
+  }
+]);
